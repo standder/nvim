ef41f7332b0212609b33e7e2e3991930c0e9ae66
standder <standder2@gmail.com>
2023-01-12 01:13:50 +0800

    update

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 
 通过toggleterm实现nvim内使用终端
 
+添加了everforest作为配色方案
+
+以packer作为插件管理器
--- lua/plugin.lua
+++ lua/plugin.lua
@@ -21,4 +21,10 @@ use {"akinsho/toggleterm.nvim", tag = '*', config = function()
   require("toggleterm").setup()
 end}
 use 'sainnhe/everforest'
+use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+})
+
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
